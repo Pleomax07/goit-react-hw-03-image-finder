a0a603d39eefa4ea51eb208da643f71859cf2ae7
Max Siomak <pleomax250193@gmail.com>
2022-11-10 23:33:09 +0200

    Update Api.jsx

--- src/components/servises/Api.jsx
+++ src/components/servises/Api.jsx
@@ -3,16 +3,23 @@ import axios from 'axios';
 const BASE_URL = 'https://pixabay.com/api';
 const KEY = '30168156-67acd7ab620ceb0f9c67e2ca5';
 
-export const GetImages = async (values, page) => {
-  const res = await axios.get(BASE_URL, {
-    params: {
-      q: values,
-      page: page,
-      key: KEY,
-      image_type: 'photo',
-      orientation: 'horizontal',
-      per_page: 12,
-    },
-  });
-  return res;
+// export const GetImages = async (searchNames, page) => {
+//   const res = await axios.get(BASE_URL, {
+//     params: {
+//       q: searchNames,
+//       page: page,
+//       key: KEY,
+//       image_type: 'photo',
+//       orientation: 'horizontal',
+//       per_page: 12,
+//     },
+//   });
+//   return res;
+// };
+
+
+export const GetImages = (searchNames, page) => {
+  return axios(
+    `https://pixabay.com/api/?q=${searchNames}&page=${page}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=12`
+  );
 };
